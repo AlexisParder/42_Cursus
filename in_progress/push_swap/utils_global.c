dcea727f94ff7a356aa0667998a15c7e7fe4fe46
AlexisParder <apollon4Gaming@outlook.fr>
2025-11-19 16:38:36 +0100

    in progress

--- in_progress/push_swap/utils_global.c
+++ in_progress/push_swap/utils_global.c
@@ -6,7 +6,7 @@
 /*   By: achauvie <achauvie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/14 12:35:26 by achauvie          #+#    #+#             */
-/*   Updated: 2025/11/19 15:12:03 by achauvie         ###   ########.fr       */
+/*   Updated: 2025/11/19 16:38:28 by achauvie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,16 +57,48 @@ static void	sort_size_3(t_stack **a)
 		rra(a, 1);
 }
 
+static void	order_stack(t_stack **a, t_stack **b)
+{
+	size_t max_a_pos;
+
+	max_a_pos = find_stack_max_pos(*a);
+	if ((*a)->nbr > (*b)->nbr)
+	{
+		if ((*b)->nbr < find_stack_min_value(*a))
+		{
+			while (find_stack_min_pos(*a) != 0)
+				rra(a, 1);
+		}
+		pa(a, b);
+	}
+	else
+	{
+		while (max_a_pos != 0)
+		{
+			rra(a, 1);
+			max_a_pos = find_stack_max_pos(*a);
+		}
+		pa(a, b);
+	}
+}
+
+// static void	check_order(t_stack **a)
+// {
+	
+// }
+
 void	push_swap(t_stack *a)
 {
 	t_stack		*b;
 	t_calc_mv	*next_nbr;
 	size_t		size_a;
+	size_t		size_b;
 
 	b = NULL;
 	pb(&a, &b);
 	pb(&a, &b);
 	size_a = ps_stacksize(a);
+	size_b = ps_stacksize(b);
 	while (size_a > 3)
 	{
 		next_nbr = calc_move(a, b);
@@ -75,10 +107,10 @@ void	push_swap(t_stack *a)
 		size_a = ps_stacksize(a);
 	}
 	sort_size_3(&a);
-	// DEBUG
-	ft_printf("\n\nSTACK A\n");
-	debug_print_stack(a);
-	ft_printf("STACK B\n");
-	debug_print_stack(b);
-	// 
+	while (size_b > 0)
+	{
+		order_stack(&a, &b);
+		size_b = ps_stacksize(b);
+	}
+	// check_order(&a);
 }
