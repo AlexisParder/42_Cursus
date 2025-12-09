fd7257aea6564159db0c69a76b7cd3f1fd3e6b88
AlexisParder <alexisparder@gmail.com>
2025-12-09 13:48:36 +0100

    modified:   so_long/srcs/main.c 	modified:   so_long/srcs/utils/utils_bonus.c

--- so_long/srcs/main.c
+++ so_long/srcs/main.c
@@ -6,13 +6,13 @@
 /*   By: achauvie <achauvie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/28 10:30:13 by achauvie          #+#    #+#             */
-/*   Updated: 2025/12/09 12:07:44 by achauvie         ###   ########.fr       */
+/*   Updated: 2025/12/09 13:14:41 by achauvie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <so_long.h>
 
-static void	key_hook(int key, void *param)
+static void	keydown_hook(int key, void *param)
 {
 	t_mlx_dt	*mlx_data;
 
@@ -22,32 +22,23 @@ static void	key_hook(int key, void *param)
 	else if (!mlx_data->key_pressed && key == 7)
 	{
 		mlx_data->key_pressed = 1;
-		make_move(mlx_data, 'r');		
+		make_move(mlx_data, 'r');
 	}
 	else if (!mlx_data->key_pressed && key == 26)
 	{
 		mlx_data->key_pressed = 1;
-		make_move(mlx_data, 't');		
+		make_move(mlx_data, 't');
 	}
 	else if (!mlx_data->key_pressed && key == 4)
 	{
 		mlx_data->key_pressed = 1;
-		make_move(mlx_data, 'l');		
+		make_move(mlx_data, 'l');
 	}
 	else if (!mlx_data->key_pressed && key == 22)
 	{
 		mlx_data->key_pressed = 1;
-		make_move(mlx_data, 'd');		
+		make_move(mlx_data, 'd');
 	}
-}
-
-static void	win_hook(int key, void *param)
-{
-	t_mlx_dt	*mlx_data;
-
-	mlx_data = (t_mlx_dt *)param;
-	if (key == 0)
-		mlx_loop_end((mlx_context)mlx_data->mlx);
 }
 
 static void	keyup_hook(int key, void *param)
@@ -57,6 +48,15 @@ static void	keyup_hook(int key, void *param)
 	mlx_data = (t_mlx_dt *)param;
 	if (key)
 		mlx_data->key_pressed = 0;
+}
+
+static void	win_hook(int key, void *param)
+{
+	t_mlx_dt	*mlx_data;
+
+	mlx_data = (t_mlx_dt *)param;
+	if (key == 0)
+		mlx_loop_end((mlx_context)mlx_data->mlx);
 }
 
 int	main(int ac, char **av)
@@ -78,7 +78,7 @@ int	main(int ac, char **av)
 	info.height = 1080;
 	info.is_resizable = true;
 	mlx_dt.win = mlx_new_window(mlx_dt.mlx, &info);
-	mlx_on_event(mlx_dt.mlx, mlx_dt.win, MLX_KEYDOWN, key_hook, &mlx_dt);
+	mlx_on_event(mlx_dt.mlx, mlx_dt.win, MLX_KEYDOWN, keydown_hook, &mlx_dt);
 	mlx_on_event(mlx_dt.mlx, mlx_dt.win, MLX_WINDOW_EVENT, win_hook, &mlx_dt);
 	mlx_on_event(mlx_dt.mlx, mlx_dt.win, MLX_KEYUP, keyup_hook, &mlx_dt);
 	creates_images(&mlx_dt, &mlx_dt.map);
--- so_long/srcs/utils/utils_bonus.c
+++ so_long/srcs/utils/utils_bonus.c
@@ -6,17 +6,20 @@
 /*   By: achauvie <achauvie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/09 12:48:43 by achauvie          #+#    #+#             */
-/*   Updated: 2025/12/09 12:54:57 by achauvie         ###   ########.fr       */
+/*   Updated: 2025/12/09 13:22:44 by achauvie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <so_long.h>
 
-void	display_move(t_mlx_dt *mlx_data)
+void	display_move(t_mlx_dt *dt)
 {
 	mlx_color	color;
+	char		*nb_move;
 
 	color.rgba = 0xFFFFFFFF;
-	mlx_string_put(mlx_data->mlx, mlx_data->win, 32, 32, color, "Number of movements:");
-	mlx_string_put(mlx_data->mlx, mlx_data->win, 192, 32, color, ft_itoa(mlx_data->player->nb_move));
+	nb_move = ft_itoa(dt->player->nb_move);
+	mlx_string_put(dt->mlx, dt->win, 32, 32, color, "Number of movements:");
+	mlx_string_put(dt->mlx, dt->win, 192, 32, color, nb_move);
+	free(nb_move);
 }
