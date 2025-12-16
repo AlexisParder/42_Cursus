f58d7279bce087513d5fc20689e3a4ba08a01b94
AlexisParder <alexisparder@gmail.com>
2025-12-16 15:10:05 +0100

    modified:   .gitignore 	renamed:    readme.md -> README.md 	renamed:    born2beroot/readme.md -> born2beroot/README.md 	renamed:    ft_printf/readme.md -> ft_printf/README.md 	renamed:    get_next_line/readme.md -> get_next_line/README.md 	renamed:    libft/readme.md -> libft/README.md 	renamed:    pipex/readme.md -> pipex/README.md 	renamed:    push_swap/readme.md -> push_swap/README.md 	renamed:    so_long/readme.md -> so_long/README.md

--- pipex/includes/pipex.h
+++ pipex/includes/pipex.h
@@ -6,7 +6,7 @@
 /*   By: achauvie <achauvie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/16 12:55:39 by achauvie          #+#    #+#             */
-/*   Updated: 2025/12/16 13:17:29 by achauvie         ###   ########.fr       */
+/*   Updated: 2025/12/16 14:43:34 by achauvie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 # include <libft.h>
 # include <fcntl.h>
+# include <stdio.h>
 # include <sys/wait.h>
 
 #endif
