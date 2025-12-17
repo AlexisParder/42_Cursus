71887e159d96bbe14f58152e99c170d4ee32af49
AlexisParder <alexisparder@gmail.com>
2025-12-17 11:58:00 +0100

    modified:   so_long/README.md

--- pipex/srcs/main.c
+++ pipex/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: achauvie <achauvie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/16 12:56:30 by achauvie          #+#    #+#             */
-/*   Updated: 2025/12/16 15:05:37 by achauvie         ###   ########.fr       */
+/*   Updated: 2025/12/17 10:26:28 by achauvie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,21 @@
 
 int main(int ac, char **av)
 {
-	int	fd1;
-	
+	int		fd1;
+	int		pipefd[2];
+	pid_t	pid1;
+
 	if (ac != 5)
 	{
 		ft_putstr_fd("Error usage: ./pipex file1 cmd1 cmd2 file2\n", 2);
 		return (1);
 	}
+	pipe(pipefd);
+	pid1 = fork();
 	fd1 = open(av[1], O_RDONLY);
 	if (fd1 < 0)
 		perror(av[1]);
+	dup2(fd1, STDIN_FILENO);
+	close (fd1);
 	return 0;
 }
