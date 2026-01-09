d5959097c3cb62dd07e1181f6cadf4d0327e0075
AlexisParder <alexisparder@gmail.com>
2026-01-09 15:42:11 +0100

    modified:   pipex/srcs/mandatory/utils/utils_cmds.c

--- pipex/srcs/mandatory/utils/utils_cmds.c
+++ pipex/srcs/mandatory/utils/utils_cmds.c
@@ -6,11 +6,37 @@
 /*   By: achauvie <achauvie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/06 09:22:58 by achauvie          #+#    #+#             */
-/*   Updated: 2026/01/09 08:44:31 by achauvie         ###   ########.fr       */
+/*   Updated: 2026/01/09 11:03:52 by achauvie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <pipex.h>
+
+static char	**split_cmds(t_pipex *data, long arg_nb)
+{
+	char	**args;
+	size_t	i;
+
+	i = 0;
+	while (data->argv[arg_nb][i])
+	{
+		if (data->argv[arg_nb][i] == '"')
+			data->argv[arg_nb][i] = ' ';
+		i++;
+	}
+	args = ft_split_all_space(data->argv[arg_nb]);
+	if (!args)
+	{
+		args = ft_calloc(1, sizeof(char *));
+		return (args);
+	}
+	// DEBUG
+	i = 0;
+	while(args[i++])
+		ft_printf("%s\n", args[i]);
+	// END DEBUG
+	return (args);
+}
 
 int	fill_cmds(t_pipex *data)
 {
@@ -32,7 +58,9 @@ int	fill_cmds(t_pipex *data)
 		if (!data->cmds[j].name)
 			return (1);
 		ft_strlcpy(data->cmds[j].name, data->argv[i], k + 1);
-		data->cmds[j].args = ft_split_all_space(data->argv[i]);
+		data->cmds[j].args = split_cmds(data, i);
+		if (!data->cmds[j].args || !data->cmds[j].args[0])
+			return (1);
 		i++;
 		j++;
 	}
