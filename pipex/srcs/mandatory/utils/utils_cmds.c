2f94606d617de92f2cb4fd7981c3992db102d8b2
AlexisParder <alexisparder@gmail.com>
2026-01-09 08:47:20 +0100

    change ft_split to ft_split_all_space

--- pipex/srcs/mandatory/utils/utils_cmds.c
+++ pipex/srcs/mandatory/utils/utils_cmds.c
@@ -6,7 +6,7 @@
 /*   By: achauvie <achauvie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/06 09:22:58 by achauvie          #+#    #+#             */
-/*   Updated: 2026/01/06 11:07:47 by achauvie         ###   ########.fr       */
+/*   Updated: 2026/01/09 08:44:31 by achauvie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ int	fill_cmds(t_pipex *data)
 		if (!data->cmds[j].name)
 			return (1);
 		ft_strlcpy(data->cmds[j].name, data->argv[i], k + 1);
-		data->cmds[j].args = ft_split(data->argv[i], ' ');
+		data->cmds[j].args = ft_split_all_space(data->argv[i]);
 		i++;
 		j++;
 	}
