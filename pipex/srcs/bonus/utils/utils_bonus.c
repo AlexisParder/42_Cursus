03f2802ed65088eee277fef919acd974b487d67e
AlexisParder <alexisparder@gmail.com>
2026-01-20 13:14:38 +0100

    fix err for check command without path

--- pipex/srcs/bonus/utils/utils_bonus.c
+++ pipex/srcs/bonus/utils/utils_bonus.c
@@ -6,7 +6,7 @@
 /*   By: achauvie <achauvie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/05 10:19:50 by achauvie          #+#    #+#             */
-/*   Updated: 2026/01/20 13:05:48 by achauvie         ###   ########.fr       */
+/*   Updated: 2026/01/20 13:14:04 by achauvie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,6 +37,13 @@ void	err_path(t_pipex *data, size_t cmd_nb)
 		ft_putstr_fd(data->cmds[cmd_nb].name, 2);
 		ft_putstr_fd(": No such file or directory\n", 2);
 	}
+	else if (data->cmds[cmd_nb].err_path == 5)
+	{
+		ft_putstr_fd(data->cmds[cmd_nb].name, 2);
+		ft_putstr_fd(": Permission denied\n", 2);
+		free_all(data);
+		exit(126);
+	}
 	else
 		perror(data->cmds[cmd_nb].name);
 	free_all(data);
@@ -45,8 +52,13 @@ void	err_path(t_pipex *data, size_t cmd_nb)
 
 int	check_without_path(t_pipex *data, int cmd_nb, char *cmd_tmp)
 {
-	if (access(cmd_tmp, X_OK) == 0)
-		data->cmds[cmd_nb].path = ft_strdup(cmd_tmp);
+	if (access(cmd_tmp, F_OK) == 0)
+	{
+		if (access(cmd_tmp, X_OK) == 0)
+			data->cmds[cmd_nb].path = ft_strdup(cmd_tmp);
+		else
+			return (5);
+	}
 	else
 		return (4);
 	return (0);
