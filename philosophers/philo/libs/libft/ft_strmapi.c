9a94c2e6705927c47597550a83d26d3dfd46028d
AlexisParder <alexisparder@gmail.com>
2026-01-22 09:26:58 +0100

    add base

---
+++ philosophers/philo/libs/libft/ft_strmapi.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: achauvie <achauvie@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/15 09:40:34 by achauvie          #+#    #+#             */
+/*   Updated: 2025/12/29 09:01:45 by achauvie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	int					s_len;
+	unsigned int		i;
+	char				*str;
+
+	if (!s || !f)
+		return (NULL);
+	s_len = ft_strlen(s);
+	str = malloc((s_len + 1) * sizeof(char));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		str[i] = (*f)(i, s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
