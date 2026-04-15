e79b6d1e725d65ec68ce8d16e658a20d03d469ed
AlexisParder <alexisparder@gmail.com>
2026-04-15 13:33:38 +0200

    add base

--- CPP/CPP00/ex02/srcs/Account.cpp
+++ CPP/CPP00/ex02/srcs/Account.cpp
@@ -6,9 +6,67 @@
 /*   By: achauvie <achauvie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/13 08:53:35 by achauvie          #+#    #+#             */
-/*   Updated: 2026/04/13 08:53:45 by achauvie         ###   ########.fr       */
+/*   Updated: 2026/04/15 13:33:10 by achauvie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <Account.hpp>
 
+Account::Account(void) {}
+
+Account::Account(int initial_deposit)
+{
+	
+}
+
+Account::~Account(void){}
+
+void	Account::_displayTimestamp(void)
+{
+	
+}
+
+int	Account::getNbAccounts(void)
+{
+	return (_nbAccounts);
+}
+
+int	Account::getTotalAmount(void)
+{
+	return (_totalAmount);
+}
+
+int	Account::getNbDeposits(void)
+{
+	return (_totalNbDeposits);
+}
+
+int	Account::getNbWithdrawals(void)
+{
+	return (_totalNbWithdrawals);
+}
+
+void	Account::displayAccountsInfos(void)
+{
+	
+}
+
+void	Account::makeDeposit(int deposit)
+{
+	
+}
+
+bool	Account::makeWithdrawal(int withdrawal)
+{
+	
+}
+
+int	Account::checkAmount(void) const
+{
+	
+}
+
+void	Account::displayStatus(void) const
+{
+	
+}
