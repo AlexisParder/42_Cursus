ed92380590d2eeab4a00751a2c6e2faeebd58f2b
AlexisParder <alexisparder@gmail.com>
2026-05-27 09:46:53 +0200

    fix: update variable naming

--- milestone_04/CPP_0-4/CPP01/ex02/srcs/main.cpp
+++ milestone_04/CPP_0-4/CPP01/ex02/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: achauvie <achauvie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/07 10:46:51 by achauvie          #+#    #+#             */
-/*   Updated: 2026/05/07 12:55:07 by achauvie         ###   ########.fr       */
+/*   Updated: 2026/05/27 09:46:33 by achauvie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,10 @@
 int	main(void)
 {
 	std::string str = "HI THIS IS BRAIN";
-	std::string *stringPTR = &str;
-	std::string &stringREF = str;
+	std::string *stringPtr = &str;
+	std::string &stringRef = str;
 
-	std::cout << &str << " / " << stringPTR << " / " << &stringREF << std::endl;
-	std::cout << str << " / " << *stringPTR << " / " << stringREF << std::endl;
+	std::cout << &str << " / " << stringPtr << " / " << &stringRef << std::endl;
+	std::cout << str << " / " << *stringPtr << " / " << stringRef << std::endl;
 	return (0);
 }
