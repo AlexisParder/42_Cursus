0d21dde87a235ae976802de27db0187a3a5bbb48
AlexisParder <alexisparder@gmail.com>
2026-05-28 11:41:26 +0200

    fix: remove unnecessary output statements in main function

--- milestone_04/CPP_0-4/CPP03/ex03/srcs/main.cpp
+++ milestone_04/CPP_0-4/CPP03/ex03/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: achauvie <achauvie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/13 11:38:02 by achauvie          #+#    #+#             */
-/*   Updated: 2026/05/14 10:25:32 by achauvie         ###   ########.fr       */
+/*   Updated: 2026/05/28 11:41:04 by achauvie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,20 +16,15 @@ int main(void)
 {
     DiamondTrap diamond("Diamond");
     
-    std::cout << "--- Attack ---" << std::endl;
     diamond.attack("enemy");
     
-    std::cout << "--- Take Damage ---" << std::endl;
     diamond.takeDamage(20);
     
-    std::cout << "--- Repair ---" << std::endl;
     diamond.beRepaired(10);
     
-    std::cout << "--- Special abilities ---" << std::endl;
     diamond.guardGate();
     diamond.highFivesGuys();
-    
-    std::cout << "--- Who am I? ---" << std::endl;
+
     diamond.whoAmI();
     
     return (0);
