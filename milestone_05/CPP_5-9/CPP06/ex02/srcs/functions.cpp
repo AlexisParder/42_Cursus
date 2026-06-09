61f77645e94fb2a8db59e6ab360fedd22a8740e1
AlexisParder <alexisparder@gmail.com>
2026-06-09 09:00:22 +0200

    replace if/else with try/catch

--- milestone_05/CPP_5-9/CPP06/ex02/srcs/functions.cpp
+++ milestone_05/CPP_5-9/CPP06/ex02/srcs/functions.cpp
@@ -6,7 +6,7 @@
 /*   By: achauvie <achauvie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/08 13:16:52 by achauvie          #+#    #+#             */
-/*   Updated: 2026/06/08 13:39:58 by achauvie         ###   ########.fr       */
+/*   Updated: 2026/06/09 08:59:46 by achauvie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,18 +32,37 @@ void identify(Base* p)
     	std::cout << "B" << std::endl;
 	else if (dynamic_cast<C*>(p) != NULL)
     	std::cout << "C" << std::endl;
-	else
-		std::cout << "Invalid type!" << std::endl;
 }
 
 void identify(Base& p)
 {
-	if (dynamic_cast<A*>(&p) != NULL)
-    	std::cout << "A" << std::endl;
-	else if (dynamic_cast<B*>(&p) != NULL)
-    	std::cout << "B" << std::endl;
-	else if (dynamic_cast<C*>(&p) != NULL)
-    	std::cout << "C" << std::endl;
-	else
-		std::cout << "Invalid type!" << std::endl;
+	try {
+		A &a = dynamic_cast<A&>(p);
+		std::cout << "A" << std::endl;
+		(void)a;
+	}
+	catch(std::exception &e) 
+	{
+		(void)e;
+	}
+
+	try {
+		B &b = dynamic_cast<B&>(p);
+		std::cout << "B" << std::endl;
+		(void)b;
+	}
+	catch(std::exception &e) 
+	{
+		(void)e;
+	}
+
+	try {
+		C &c = dynamic_cast<C&>(p);
+		std::cout << "C" << std::endl;
+		(void)c;
+	}
+	catch(std::exception &e) 
+	{
+		(void)e;
+	}
 }
